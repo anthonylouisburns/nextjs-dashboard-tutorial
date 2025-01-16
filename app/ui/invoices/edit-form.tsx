2f1257775867f0ea7d176ede26268340c36d144c
Anthony Louis Burns, Esq <burnstony@gmail.com>
2025-01-16 18:04:56 -0500

    finished tutorial

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -25,7 +25,8 @@ export default function EditInvoiceForm({
   // const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+  // const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+  const [formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
     <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
