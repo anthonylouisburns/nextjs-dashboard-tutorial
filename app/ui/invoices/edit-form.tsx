e20e5139facc3c10e2bd81dcee660f9161988055
Anthony Louis Burns, Esq <burnstony@gmail.com>
2025-01-16 22:03:43 -0500

    finished tutorial

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -20,7 +20,7 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
 
-  const initialState: State = { message: null, errors: {} };
+  // const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   // const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
