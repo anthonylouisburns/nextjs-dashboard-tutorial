e715d108f8c8ed78ca672b0c8878e6aab7302ef2
Anthony Louis Burns, Esq <burnstony@gmail.com>
2025-01-16 22:01:49 -0500

    finished tutorial

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,7 +10,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { useActionState } from 'react';
+// import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
